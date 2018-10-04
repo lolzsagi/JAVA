ade202eb0c10ac414062766766a6b414e46eea00
sagarswamirao <35551919+sagarswamirao@users.noreply.github.com>
2018-10-04 16:28:26 +0530

    Add files via upload

---
+++ constructo.java
@@ -0,0 +1,50 @@
+class HOSPITAL
+{
+HOSPITAL()
+{
+int i=6;
+System.out.println("Constructor called & value of i initialized is"i);
+}
+}
+class COLUMBIA extends HOSPITAL
+{
+void patients()
+{
+System.out.println("COLUMBIA-ASIA:10 PATIENTS");
+}
+}
+class APOLLO extends HOSPITAL
+{
+void patients()
+{
+System.out.println("APOLLO:20 PATIENTS");
+}
+}
+class MANIPAL extends HOSPITAL
+{
+void patients()
+{
+System.out.println("MANIPAL: 15 PATIENTS");
+}
+}
+class NIMHANS extends HOSPITAL
+{
+void patients()
+{
+System.out.println("NIMHANS: 40 PATIENTS");
+}
+}
+class constructo
+{
+public static void main(String args[])
+{
+COLUMBIA obj=new COLUMBIA();
+APOLLO obj1=new APOLLO();
+MANIPAL obj2=new MANIPAL();
+NIMHANS obj3=new NIMHANS();
+obj.patients();
+obj1.patients();
+obj2.patients();
+obj3.patients();
+}
+}
